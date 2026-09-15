a5e3fcd5d89fb54867a4872ae7323fb05e74ef62
althea000 <86103185+althea000@users.noreply.github.com>
2026-09-15 16:46:10 +0800

    Done with Report and Compliance API

--- Backend/Server.js
+++ Backend/Server.js
@@ -15,6 +15,7 @@ import posRouter from "./Routes/pos.js";
 import restockRouter from "./Routes/restock.js";
 import arRouter from "./Routes/ar.js";
 import mobileHomeRouter from "./Routes/mobileHome.js";
+import reportsRouter from "./Routes/reports.js";
 
 
 dotenv.config();
@@ -43,6 +44,7 @@ app.use(`${API_PREFIX}/deliveries`, deliveriesRouter);
 app.use(`${API_PREFIX}/pos`, posRouter);
 app.use(`${API_PREFIX}/ar`, arRouter);
 app.use(`${API_PREFIX}/mobile`, mobileHomeRouter);
+app.use(`${API_PREFIX}/reports`, reportsRouter);
 
 app.get("/", (req, res) => {
     res.send("GasTrack API is running.");
